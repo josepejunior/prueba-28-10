9932c629431d3f4f10145d61b38de393a1d88359
josepejunior <90076382+josepejunior@users.noreply.github.com>
2021-10-28 12:06:09 -0600

    Add files via upload

---
+++ AppCore/Interfaces/IActivoService.cs
@@ -0,0 +1,20 @@
+﻿using Domain.Activo_Fijo;
+using Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppCore.Interfaces
+{
+    public interface IActivoService : IService<ActivoFijo>
+    {
+        ActivoFijo GetActivoById(int id);
+        ActivoFijo GetActivoFijo(int id);
+        ActivoFijo[] GetActivoByTipodeActivo(TipoActivoFijo um);
+        ActivoFijo[] GetActivoByFechadeAdquisicion(DateTime dt);
+        ActivoFijo[] GetActivosByRangoValor(decimal start, decimal end);
+        string GetActivosAsJson();
+        ActivoFijo[] GetActivosOrderByValor();
+        int GetLastActivoId();
+    }
+}
